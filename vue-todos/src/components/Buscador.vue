808dcc03ad09cc84645349e8972ca3ad1bd543ea
Alex10161010 <eliasalejandromome@gmail.com>
2021-05-20 17:15:40 -0500

    registro de tareas con vue

---
+++ vue-todos/src/components/Buscador.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="buscador-container">
+        <input type="text" v-model="query" v-on:input="$emit('query-change',query)">
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'Buscador',
+    data() {
+        return {
+            query:''
+        }
+    },
+}
+</script>
+
+<style scoped>
+    #buscador-container{
+        width: 600px;
+        margin: 0 auto;
+    }
+    input{
+        padding: 10px;
+        width: 100%;
+        outline: none;
+    }
+</style>
