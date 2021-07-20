53a6407c85b15c6cd8742ed028a85c320e95acde
Alex10161010 <eliasalejandromome@gmail.com>
2021-07-20 15:56:40 -0500

    elimina este archivo para realizar un formulario

--- vue-cli-prueba/src/main.js
+++
@@ -1,6 +0,0 @@
-/* Importamos la aplicacion de vue */
-import { createApp } from 'vue';
-/* Importamos el componete de app */
-import App from './App.vue';
-/* Montamos el componente en el identificador #app*/
-createApp(App).mount('#app');
